6f35fd1e1a59baf013389f89478aee2875817e4d
Heitor <mchheitor@gmail.com>
2024-04-17 10:49:24 -0300

    fixed

--- transformskip.vhd
+++ transformskip.vhd
@@ -105,48 +105,60 @@ begin
         reg15_neibor <= "0000000000000000";
 
         
-        --15 13 10 6 
-        --14 11 7 3
-        --12 8 4 1
-        --9 5 2 0
+        
 
+        --0 2 5 9 
+        --1 4 8 12
+        --3 7 11 14
+        --6 10 13 15
 
     elsif (rising_edge(clock)) then
-        if escolha ="0000" then ??
-
-        if escolha="0001" OR escolha "0010" then
-            reg0_neibor <= coeff+reg0_neibor;
-        if escolha="0011" OR escolha "0100" then
-            reg1_neibor <= coeff+reg1_neibor;
-        if escolha="0101" OR escolha "0100" then
-            reg2_neibor <= coeff+reg2_neibor;
-        if escolha="0111" OR escolha "0110" then
-            reg3_neibor <= coeff+reg3_neibor;
-        if escolha="0111" OR escolha "1000" then
-            reg4_neibor <= coeff+reg4_neibor;
-        if escolha="1001" OR escolha "1000" then
-            reg5_neibor <= coeff+reg5_neibor;
-        if escolha="1010" 
-            reg6_neibor <= coeff+reg6_neibor;
-        if escolha="1011" OR escolha "1010" then
-            reg7_neibor <= coeff+reg7_neibor;
-        if escolha="1100" OR escolha "1011" then
-            reg8_neibor <= coeff+reg8_neibor;
-        if escolha="1100" 
-            reg9_neibor <= coeff+reg9_neibor;
-        if escolha="1101" 
-            reg10_neibor <= coeff+reg10_neibor;
-        if escolha="1110" OR escolha "1101" then
-            reg11_neibor <= coeff+reg11_neibor;
-        if escolha "1110" then
-            reg12_neibor <= coeff+reg12_neibor;
-        if escolha="1111"
-            reg13_neibor <= coeff+reg13_neibor;
-            reg14_neibor <= coeff+reg14_neibor;
-        if escolha ?
-            reg15_neibor <= coeff+reg15_neibor;
-        end if;
-    end if;
+        if escolha=="0000" then
+                reg0_neibor <= reg0_neibor;
+                reg1_neibor <= reg1_neibor+coeff;
+                reg2_neibor <= reg2_neibor+coeff;
+                end if;
+        if escolha=="0001" then
+                reg3_neibor <= reg3_neibor+coeff;
+                end if;
+        if escolha=="0010" OR escolha=="0001" then
+                reg4_neibor <= reg4_neibor+coeff;
+                end if;
+        if escolha=="0010" then
+                reg5_neibor <= reg5_neibor+coeff;
+                end if;
+        if escolha=="0011" then
+                reg6_neibor <= reg6_neibor+coeff;
+                end if;
+        if escolha=="0100" OR escolha=="0011"then
+                reg7_neibor <= reg7_neibor+coeff;
+                end if;
+        if escolha=="0100" OR escolha=="0101" then
+                reg8_neibor <= reg8_neibor+coeff;
+                end if;
+        if escolha=="0101" then
+                reg9_neibor <= reg9_neibor+coeff;
+                end if;
+        if escolha=="0110" OR escolha=="0111" then
+                reg10_neibor <= reg10_neibor+coeff;
+                end if;
+        if escolha=="1000" OR escolha=="0111"then
+                reg11_neibor <= reg11_neibor+coeff;
+                end if;
+        if escolha=="1000" OR escolha=="1001" then
+                reg12_neibor <= reg12_neibor+coeff;
+                end if;
+        if escolha=="1011" OR escolhar=="1010" then
+                reg13_neibor <= reg13_neibor+coeff;
+                end if;
+        if escolha=="1011" OR escolha=="1100" then
+                reg14_neibor <= reg14_neibor+coeff;
+                end if;
+        if escolha=="1110" OR escolha=="1101"then
+                reg15_neibor <= reg15_neibor+coeff;
+                end if;
+                
+      end if;
 end process;
 
 pos0_0<=regpos0_menor(1) when sk<"10" AND M0="01"else
